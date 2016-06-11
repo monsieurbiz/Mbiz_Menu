f9e996bda1d79a32940b5f1aad43077d61fdec7b
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2016-06-11 17:04:29 +0200

    Add source code :kissing_heart:

---
+++ app/code/community/Mbiz/Menu/Block/Page/Html/Topmenu.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * This file is part of Mbiz_Menu for Magento.
+ *
+ * @license GPL-3.0
+ * @author Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com> <@jacquesbh>
+ * @category Mbiz
+ * @package Mbiz_Menu
+ * @copyright Copyright (c) 2016 Monsieur Biz (https://monsieurbiz.com/)
+ */
+
+/**
+ * Page_Html_Topmenu Block
+ * @package Mbiz_Menu
+ */
+class Mbiz_Menu_Block_Page_Html_Topmenu extends Mage_Core_Block_Template
+{
+
+    /**
+     * Categories
+     * @var array
+     */
+    protected $_categories;
+
+    /**
+     * @inheritDoc
+     */
+    public function getCacheLifetime()
+    {
+        return parent::getCacheLifetime() !== null
+            ? parent::getCacheLifetime()
+            : 259200; //72h in seconds
+    }
+
+    /**
+     * Give all categories of the current store
+     * @return array Array of all categories
+     */
+    public function getCategories()
+    {
+        if (null === $this->_categories) {
+
+            $collection = Mage::getModel('catalog/category')->getCollection();
+
+            $pathLike = Mage_Catalog_Model_Category::TREE_ROOT_ID . '/' . Mage::app()->getStore()->getRootCategoryId() . '/%';
+            $collection
+                ->initCache(Mage::app()->getCacheInstance(), self::class, [Mage_Catalog_Model_Category::CACHE_TAG])
+                ->setStoreId(Mage::app()->getStore()->getId())
+                ->addAttributeToSelect('*')
+                ->addFieldToFilter('path', [
+                    'like' => $pathLike,
+                ])
+                ->addAttributeToFilter('include_in_menu', 1);
+
+            // Sort
+            $collection->getSelect()->reset(Zend_Db_Select::ORDER);
+            $collection
+                ->addOrder('position', Varien_Data_Collection_Db::SORT_ORDER_ASC);
+
+            foreach ($collection as $id => $category) {
+                $this->_categories[$id] = $category;
+            }
+        }
+
+        return $this->_categories;
+    }
+
+    /**
+     * Retrieve the categories tree
+     * @return array Array of the main categories with their children
+     */
+    public function getCategoriesTree()
+    {
+        $allCategories = $this->getCategories();
+        $topCategories = [];
+
+        // Building tree from categories collection
+        foreach ($allCategories as $idCat => $category) {
+
+            // If level, we put category in an array with only top level categories.
+            $topLevel = 2; // 1 is root category
+            if ($category->getLevel() == $topLevel && !isset($topCategories[$idCat])) {
+                $topCategories[$idCat] = $category;
+            } else {
+                // Foreach categories with sub category we add a child to the parent
+                if (!isset($allCategories[$category->getParentId()])) {
+                    // Ignore if no parent
+                    continue;
+                }
+                $parent           = $allCategories[$category->getParentId()];
+                $children         = $parent->getData('_children') ?: [];
+                $children[$idCat] = $category;
+                $parent->setData('_children', $children);
+            }
+        }
+
+        return $topCategories;
+    }
+
+}
